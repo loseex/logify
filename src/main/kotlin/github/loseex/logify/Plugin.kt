4473fe5fd8d8aaf0b575d9f29d9fb27382469249
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2025-08-12 16:53:37 +0500

    Started to develop memory storage. - Created a storage class in the project. - Added a connection with an instance of the plugin.

--- src/main/kotlin/github/loseex/logify/Plugin.kt
+++ src/main/kotlin/github/loseex/logify/Plugin.kt
@@ -7,10 +7,13 @@ class Plugin : JavaPlugin() {
   companion object {
     public lateinit var instance: Plugin
       private set
+    public lateinit var storage: Storage
+      private set
   }
 
   override fun onEnable() {
     instance = this
+    storage = Storage()
 
     this.saveDefaultConfig()
   }
---
+++ src/main/kotlin/github/loseex/logify/Storage.kt
@@ -0,0 +1,4 @@
+package github.loseex.logify
+
+class Storage {
+}
